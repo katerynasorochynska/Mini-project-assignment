928c287ef5229b2fb0af25000679c5c55b481ab5
Mariia Zahorui <mariia.zahorui.25@kse.org.ua>
2026-07-22 13:15:29 +0300

    Розрахунок SMA та LMA

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import yfinance as yf
-data = yf.download(tickers='GOOGL', period='2d', interval='1d')
-print(data.head())
+data = yf.download(tickers='GOOGL', period='2y', interval='1d')
 # print(type(data))
+
+import pandas as pd
+data['SMA']=data['Close'].rolling(20).mean()
+data['LMA']=data['Close'].rolling(50).mean()
+print(data.head(51))
+
+
