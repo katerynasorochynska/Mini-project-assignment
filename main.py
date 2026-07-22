85079d0e7f754808b721063e3d8243660cf6fcfa
sorocinskakaterina <kateryna.sorochynska.25@kse.org.ua>
2026-07-22 16:24:00 +0300

    Додано торгові сигнали SMA/LMA

--- main.py
+++ main.py
@@ -7,4 +7,16 @@ data['SMA']=data['Close'].rolling(20).mean()
 data['LMA']=data['Close'].rolling(50).mean()
 print(data.head(51))
 
+data.columns = data.columns.get_level_values(0)
+decisions = []
 
+for i, row in data.iterrows():
+    if pd.isna(row["SMA"]) or pd.isna(row["LMA"]):
+        decisions.append('Утримуємось')
+    elif row['SMA'] > row['LMA']:
+        decisions.append('Купуємо')
+    else:
+        decisions.append('Продаємо')
+
+data['Рішення'] = decisions
+print(data)
